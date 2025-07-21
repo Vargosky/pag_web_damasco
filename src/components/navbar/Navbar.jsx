75844b03a16ed4e83f2e97f8bf64f41d5153b132
Vargosky <sarem.vargas@gmail.com>
2025-07-20 20:24:30 -0400

    change to Link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -7,7 +8,7 @@ export default function Navbar() {
   return (
     <header className="fixed top-0 left-0 w-full z-30 bg-[#F29325] shadow-md">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-        <h1 className="text-xl md:text-2xl font-bold text-[#6d2e46]">Pastelería Damasco</h1>
+        <h1 className="text-xl md:text-2xl font-bold text-[#6d2e46]"> <Link href={"/"}>Pastelería Damasco</Link> </h1>
         <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden focus:outline-none">
           <svg
             className="w-6 h-6 text-[#6d2e46]"
@@ -20,18 +21,18 @@ export default function Navbar() {
           </svg>
         </button>
         <nav className="hidden md:flex space-x-6 text-sm md:text-base">
-          <a href="/" className="text-[#6d2e46] hover:text-white transition">Inicio</a>
-          <a href="/nosotros" className="text-[#6d2e46] hover:text-white transition">Nosotros</a>
-          <a href="/productos" className="text-[#6d2e46] hover:text-white transition">Productos</a>
-          <a href="/contacto" className="text-[#6d2e46] hover:text-white transition">Contacto</a>
+          <Link href="/" className="text-[#6d2e46] hover:text-white transition">Inicio</Link>
+          <Link href="/nosotros" className="text-[#6d2e46] hover:text-white transition">Nosotros</Link>
+          <Link href="/productos" className="text-[#6d2e46] hover:text-white transition">Productos</Link>
+          <Link href="/contacto" className="text-[#6d2e46] hover:text-white transition">Contacto</Link>
         </nav>
       </div>
       {menuOpen && (
         <div className="md:hidden px-4 pb-4 space-y-2 bg-[#fff0f5]">
-          <a href="/" className="block text-[#6d2e46] hover:text-white">Inicio</a>
-          <a href="/nosotros" className="block text-[#6d2e46] hover:text-white">Nosotros</a>
-          <a href="/productos" className="block text-[#6d2e46] hover:text-white">Productos</a>
-          <a href="/contacto" className="block text-[#6d2e46] hover:text-white">Contacto</a>
+          <Link href="/" className="block text-[#6d2e46] hover:text-white">Inicio</Link>
+          <Link href="/nosotros" className="block text-[#6d2e46] hover:text-white">Nosotros</Link>
+          <Link href="/productos" className="block text-[#6d2e46] hover:text-white">Productos</Link>
+          <Link href="/contacto" className="block text-[#6d2e46] hover:text-white">Contacto</Link>
         </div>
       )}
     </header>
